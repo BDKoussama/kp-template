17dcbf989a17e1ae31037339cf2d495f88193116
BDKoussama <bdk.oussama@gmail.com>
2019-01-29 22:37:23 +0400

    project detail transition

--- source/js/app.js
+++ source/js/app.js
@@ -289,6 +289,77 @@ var AboutTransition = Barba.BaseTransition.extend({
 
 });
 
+var ProjectTransition = Barba.BaseTransition.extend({
+  start: function () {
+      isAnimating = true;
+      Promise
+          .all([this.newContainerLoading, this.scrollTop()])
+          .then(this.display.bind(this));
+  },
+
+  // scrollTop returns a promise so the before toggling next animation it waits until promise is resolved and scrolltop = 0 
+  scrollTop: function () {
+      var deferred = Barba.Utils.deferred();
+      var obj = {
+          y: window.pageYOffset
+      };
+
+      TweenLite.to(obj, 0.4, {
+          y: 0,
+          onUpdate: function () {
+              if (obj.y === 0) {
+                  deferred.resolve();
+              }
+
+              window.scroll(0, obj.y);
+          },
+          onComplete: function () {
+              deferred.resolve();
+          }
+      });
+
+      return deferred.promise;
+  },
+
+  display: function () {
+      var _this = this;
+      var tl = new TimelineMax({
+          onComplete: function () {
+              _this.newContainer.style.position = 'static';
+              _this.done();
+              isAnimating = false;
+          }
+      });
+      TweenLite.set(this.newContainer, {
+          position: 'fixed',
+          visibility: 'visible',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          opacity: 0,
+      });
+      let bannerImg = this.newContainer.querySelector('.banner-img');
+      TweenLite.set(bannerImg, {
+          scale: 1.5
+      });
+      console.log(bannerImg)
+      tl.add('start');
+      tl.to(this.oldContainer, 1.2, {
+          opacity: 0
+      }, 'start');
+      tl.add('next');
+      tl.to(this.newContainer, 1, {
+          opacity: 1
+      }, 'next');
+      tl.add('after');
+      tl.to(bannerImg, 1.5, {
+          scale: 1,
+          ease: Circ.easeOut
+      }, 'after');
+  }
+});
+
   var html = document.documentElement;
   var body = document.body;
 

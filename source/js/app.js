99b23c6c2e34b6e87d536eb86e302da9e5809445
BDKoussama <bdk.oussama@gmail.com>
2019-01-29 23:34:53 +0400

    smoothscroll bug fixed

--- source/js/app.js
+++ source/js/app.js
@@ -11,15 +11,84 @@ import SlideShow from './modules/SlideShow';
 import Cursor from './modules/Cursor';
 import Carousel from './modules/Carousel';
 document.addEventListener("DOMContentLoaded", function (event) {
-
+  var html = document.documentElement;
+  var body = document.body;
   // Barbajs Initialisation ***********************//
   Barba.Pjax.Cache.reset()
   Barba.Pjax.init();
   Barba.Prefetch.init();
   Barba.Dispatcher.on('newPageReady', function (currentStatus, oldStatus, container) {
-    //const slideShow = container.querySelector('.viewport').getAttribute('data-page') === 'index-page' ? new SlideShow(container.querySelector('.main-content')) : null;
+    const slideShow = container.querySelector('.viewport').getAttribute('data-page') === 'index-page' ? new SlideShow(container.querySelector('.main-content')) : null;
     //const carousel = container.querySelector('.viewport').getAttribute('data-page') === 'works-page' ? new Carousel(container.querySelector('.carousel-holder')) : null ; 
-    //const cursor = new Cursor()
+    const cursor = new Cursor();
+    
+
+    var scroller = {
+      target: container.querySelector("#scroll-container"),
+      ease: 0.05, // <= scroll speed
+      endY: 0,
+      y: 0,
+      resizeRequest: 1,
+      scrollRequest: 0,
+    };
+
+    var requestId = null;
+
+    TweenLite.set(scroller.target, {
+      rotation: 0.01,
+      force3D: true
+    });
+
+    //window.addEventListener("load", onLoad);
+    onLoad() 
+    function onLoad() {
+      console.log('Loaded');
+      updateScroller();
+      window.focus();
+      window.addEventListener("resize", onResize);
+      document.addEventListener("scroll", onScroll);
+    }
+
+    function updateScroller() {
+
+      var resized = scroller.resizeRequest > 0;
+
+      if (resized) {
+        var height = scroller.target.clientHeight;
+        body.style.height = height + "px";
+        scroller.resizeRequest = 0;
+      }
+
+      var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+
+      scroller.endY = scrollY;
+      scroller.y += (scrollY - scroller.y) * scroller.ease;
+
+      if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+        scroller.y = scrollY;
+        scroller.scrollRequest = 0;
+      }
+
+      TweenLite.set(scroller.target, {
+        y: -scroller.y
+      });
+
+      requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+    }
+
+    function onScroll() {
+      scroller.scrollRequest++;
+      if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller);
+      }
+    }
+
+    function onResize() {
+      scroller.resizeRequest++;
+      if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller);
+      }
+    }
   });
 
   // *********************************************// 
@@ -48,385 +117,340 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   links.forEach(link => link.addEventListener('click ', cbk))
 
-// Barbajs Transitions ************************* //
+  // Barbajs Transitions ************************* //
 
-var CommonTransition = Barba.BaseTransition.extend({
-  start: function () {
+  var CommonTransition = Barba.BaseTransition.extend({
+    start: function () {
       isAnimating = true;
       Promise
-          .all([this.newContainerLoading, this.scrollTop()])
-          .then(this.display.bind(this));
-  },
-  
-  // scrollTop returns a promise so the before toggling next animation it waits until promise is resolved and scrolltop = 0 
-  scrollTop: function () {
+        .all([this.newContainerLoading, this.scrollTop()])
+        .then(this.display.bind(this));
+    },
+
+    // scrollTop returns a promise so the before toggling next animation it waits until promise is resolved and scrolltop = 0 
+    scrollTop: function () {
       var deferred = Barba.Utils.deferred();
       var obj = {
-          y: window.pageYOffset
+        y: window.pageYOffset
       };
 
       TweenLite.to(obj, 0.4, {
-          y: 0,
-          onUpdate: function () {
-              if (obj.y === 0) {
-                  deferred.resolve();
-              }
-
-              window.scroll(0, obj.y);
-          },
-          onComplete: function () {
-              deferred.resolve();
+        y: 0,
+        onUpdate: function () {
+          if (obj.y === 0) {
+            deferred.resolve();
           }
+
+          window.scroll(0, obj.y);
+        },
+        onComplete: function () {
+          deferred.resolve();
+        }
       });
 
       return deferred.promise;
-  },
+    },
 
-  display: function () {
+    display: function () {
       var _this = this;
       var tl = new TimelineMax({
-          onComplete: function () {
-              _this.newContainer.style.position = 'static';
-               isAnimating = false;
-              _this.done();
-          }
+        onComplete: function () {
+          _this.newContainer.style.position = 'static';
+          isAnimating = false;
+          _this.done();
+        }
       });
       TweenLite.set(this.newContainer, {
-          position: 'fixed',
-          visibility: 'visible',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          opacity: 0,
+        position: 'fixed',
+        visibility: 'visible',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        opacity: 0,
       });
       tl.add('start');
       tl.to(this.oldContainer, 1.2, {
-          opacity: 0
+        opacity: 0
       }, 'start');
       //tl.add('start');
       tl.to(this.newContainer, 1, {
-          opacity: 1
+        opacity: 1
       }, '+=start');
-  }
-});
+    }
+  });
 
-var ContactTransition = Barba.BaseTransition.extend({
-  start: function () {
+  var ContactTransition = Barba.BaseTransition.extend({
+    start: function () {
       isAnimating = true;
       Promise
-          .all([this.newContainerLoading, this.scrollTop()])
-          .then(this.display.bind(this));
-  },
-  // scrollTop returns a promise so the before toggling next animation it waits until promise is resolved and scrolltop = 0 
-  scrollTop: function () {
+        .all([this.newContainerLoading, this.scrollTop()])
+        .then(this.display.bind(this));
+    },
+    // scrollTop returns a promise so the before toggling next animation it waits until promise is resolved and scrolltop = 0 
+    scrollTop: function () {
       var deferred = Barba.Utils.deferred();
       var obj = {
-          y: window.pageYOffset
+        y: window.pageYOffset
       };
 
       TweenLite.to(obj, 0.4, {
-          y: 0,
-          onUpdate: function () {
-              if (obj.y === 0) {
-                  deferred.resolve();
-              }
-
-              window.scroll(0, obj.y);
-          },
-          onComplete: function () {
-              deferred.resolve();
+        y: 0,
+        onUpdate: function () {
+          if (obj.y === 0) {
+            deferred.resolve();
           }
+
+          window.scroll(0, obj.y);
+        },
+        onComplete: function () {
+          deferred.resolve();
+        }
       });
 
       return deferred.promise;
-  },
+    },
 
-  display: function () {
+    display: function () {
       var _this = this;
       var tl = new TimelineMax({
-          onComplete: function () {
-              _this.newContainer.style.position = 'static';
-              _this.done();
-              isAnimating = false;
-          }
+        onComplete: function () {
+          _this.newContainer.style.position = 'static';
+          _this.done();
+          isAnimating = false;
+        }
       });
       let sectionTitle = this.newContainer.querySelector('.section-title h1');
       let contactInfo = this.newContainer.querySelector('.contact-info');
       TweenLite.set(this.newContainer, {
-          position: 'fixed',
-          visibility: 'visible',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          opacity: 0,
+        position: 'static',
+        visibility: 'visible',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        opacity: 0,
       });
       TweenLite.set(sectionTitle, {
-          yPercent: 120
+        yPercent: 120
       })
       TweenLite.set(contactInfo, {
-          yPercent: 150
+        yPercent: 150
       })
 
       tl.add('start');
       tl.to(this.oldContainer, 1.2, {
-          autoAlpha: 0
+        autoAlpha: 0
       }, 'start');
       tl.add('next');
       tl.to(this.newContainer, 1, {
-          opacity: 1
+        opacity: 1
       }, 'next');
       tl.add('after')
       tl.to(sectionTitle, 1.5, {
-          yPercent: 0,
-          ease: Circ.easeOut,
-          force3D: false
+        yPercent: 0,
+        ease: Circ.easeOut,
+        force3D: false
       }, "after");
       tl.to(contactInfo, 1.5, {
-          yPercent: 0,
-          ease: Circ.easeOut,
-          force3D: false
+        yPercent: 0,
+        ease: Circ.easeOut,
+        force3D: false
       }, "after");
 
-  }
+    }
 
-});
-var AboutTransition = Barba.BaseTransition.extend({
-  start: function () {
+  });
+  var AboutTransition = Barba.BaseTransition.extend({
+    start: function () {
       isAnimating = true;
       Promise
-          .all([this.newContainerLoading, this.scrollTop()])
-          .then(this.display.bind(this));
-  },
-  // scrollTop returns a promise so the before toggling next animation it waits until promise is resolved and scrolltop = 0 
-  scrollTop: function () {
+        .all([this.newContainerLoading, this.scrollTop()])
+        .then(this.display.bind(this));
+    },
+    // scrollTop returns a promise so the before toggling next animation it waits until promise is resolved and scrolltop = 0 
+    scrollTop: function () {
       var deferred = Barba.Utils.deferred();
       var obj = {
-          y: window.pageYOffset
+        y: window.pageYOffset
       };
 
       TweenLite.to(obj, 0.4, {
-          y: 0,
-          onUpdate: function () {
-              if (obj.y === 0) {
-                  deferred.resolve();
-              }
-
-              window.scroll(0, obj.y);
-          },
-          onComplete: function () {
-              deferred.resolve();
+        y: 0,
+        onUpdate: function () {
+          if (obj.y === 0) {
+            deferred.resolve();
           }
+
+          window.scroll(0, obj.y);
+        },
+        onComplete: function () {
+          deferred.resolve();
+        }
       });
 
       return deferred.promise;
-  },
+    },
 
-  display: function () {
+    display: function () {
       var _this = this;
       var tl = new TimelineMax({
-          onComplete: function () {
-              _this.newContainer.style.position = 'static';
-              _this.done();
-              isAnimating = false;
-          }
+        onComplete: function () {
+          _this.newContainer.style.position = 'static';
+          _this.done();
+          isAnimating = false;
+        }
       });
       let sectionTitle = this.newContainer.querySelector('.section-title h1');
       let hLayer = this.newContainer.querySelector('.h-layer');
       let aboutText = this.newContainer.querySelector('.about-text');
       let heroName = this.newContainer.querySelector('.h-name h1');
       TweenLite.set(this.newContainer, {
-          position: 'fixed',
-          visibility: 'visible',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          opacity: 0,
+        position: 'fixed',
+        visibility: 'visible',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        opacity: 0,
       });
       TweenLite.set(sectionTitle, {
-          yPercent: 120
+        yPercent: 120
       })
       TweenLite.set(aboutText, {
-          yPercent: 100
+        yPercent: 100
       })
       TweenLite.set(hLayer, {
-          xPercent: -101,
-          opacity: 1
+        xPercent: -101,
+        opacity: 1
       })
       TweenLite.set(heroName, {
-          opacity: 0
+        opacity: 0
       })
       tl.add('start');
       tl.to(this.oldContainer, 1, {
-          autoAlpha: 0
+        autoAlpha: 0
       }, 'start');
       tl.add('next');
       tl.to(this.newContainer, 1, {
-          autoAlpha: 1
+        autoAlpha: 1
       }, 'next');
       tl.add('after')
       tl.to(sectionTitle, 1.5, {
-          yPercent: 0,
-          ease: Circ.easeOut,
-          force3D: false
+        yPercent: 0,
+        ease: Circ.easeOut,
+        force3D: false
       }, "after");
       tl.to(aboutText, 1.5, {
-          yPercent: 0,
-          ease: Circ.easeOut,
-          force3D: false
+        yPercent: 0,
+        ease: Circ.easeOut,
+        force3D: false
       }, "after")
       tl.to(hLayer, 1.5, {
-          xPercent: 110,
-          ease: Circ.easeOut,
-          force3D: false
+        xPercent: 110,
+        ease: Circ.easeOut,
+        force3D: false
       }, "after")
       tl.to(heroName, 1, {
-          opacity: 1
+        opacity: 1
       }, "after")
-  }
+    }
 
-});
+  });
 
-var ProjectTransition = Barba.BaseTransition.extend({
-  start: function () {
+  var ProjectTransition = Barba.BaseTransition.extend({
+    start: function () {
       isAnimating = true;
       Promise
-          .all([this.newContainerLoading, this.scrollTop()])
-          .then(this.display.bind(this));
-  },
+        .all([this.newContainerLoading, this.scrollTop()])
+        .then(this.display.bind(this));
+    },
 
-  // scrollTop returns a promise so the before toggling next animation it waits until promise is resolved and scrolltop = 0 
-  scrollTop: function () {
+    // scrollTop returns a promise so the before toggling next animation it waits until promise is resolved and scrolltop = 0 
+    scrollTop: function () {
       var deferred = Barba.Utils.deferred();
       var obj = {
-          y: window.pageYOffset
+        y: window.pageYOffset
       };
 
       TweenLite.to(obj, 0.4, {
-          y: 0,
-          onUpdate: function () {
-              if (obj.y === 0) {
-                  deferred.resolve();
-              }
-
-              window.scroll(0, obj.y);
-          },
-          onComplete: function () {
-              deferred.resolve();
+        y: 0,
+        onUpdate: function () {
+          if (obj.y === 0) {
+            deferred.resolve();
           }
+
+          window.scroll(0, obj.y);
+        },
+        onComplete: function () {
+          deferred.resolve();
+        }
       });
 
       return deferred.promise;
-  },
+    },
 
-  display: function () {
+    display: function () {
       var _this = this;
       var tl = new TimelineMax({
-          onComplete: function () {
-              _this.newContainer.style.position = 'static';
-              _this.done();
-              isAnimating = false;
-          }
+        onComplete: function () {
+          _this.newContainer.style.position = 'static';
+          _this.done();
+          isAnimating = false;
+        }
       });
       TweenLite.set(this.newContainer, {
-          position: 'fixed',
-          visibility: 'visible',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          opacity: 0,
+        position: 'fixed',
+        visibility: 'visible',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        opacity: 0,
       });
       let bannerImg = this.newContainer.querySelector('.banner-img');
       TweenLite.set(bannerImg, {
-          scale: 1.5
+        scale: 1.5
       });
       console.log(bannerImg)
       tl.add('start');
       tl.to(this.oldContainer, 1.2, {
-          opacity: 0
+        opacity: 0
       }, 'start');
       tl.add('next');
       tl.to(this.newContainer, 1, {
-          opacity: 1
+        opacity: 1
       }, 'next');
       tl.add('after');
       tl.to(bannerImg, 1.5, {
-          scale: 1,
-          ease: Circ.easeOut
+        scale: 1,
+        ease: Circ.easeOut
       }, 'after');
-  }
-});
-
-  var html = document.documentElement;
-  var body = document.body;
-
-  var scroller = {
-    target: document.querySelector("#scroll-container"),
-    ease: 0.05, // <= scroll speed
-    endY: 0,
-    y: 0,
-    resizeRequest: 1,
-    scrollRequest: 0,
-  };
-
-  var requestId = null;
-
-  TweenLite.set(scroller.target, {
-    rotation: 0.01,
-    force3D: true
+    }
   });
 
-  window.addEventListener("load", onLoad);
-
-  function onLoad() {
-    updateScroller();
-    window.focus();
-    window.addEventListener("resize", onResize);
-    document.addEventListener("scroll", onScroll);
-  }
-
-  function updateScroller() {
-
-    var resized = scroller.resizeRequest > 0;
-
-    if (resized) {
-      var height = scroller.target.clientHeight;
-      body.style.height = height + "px";
-      scroller.resizeRequest = 0;
-    }
-
-    var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
-
-    scroller.endY = scrollY;
-    scroller.y += (scrollY - scroller.y) * scroller.ease;
-
-    if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-      scroller.y = scrollY;
-      scroller.scrollRequest = 0;
-    }
-
-    TweenLite.set(scroller.target, {
-      y: -scroller.y
+  Barba.Pjax.getTransition = function () {
+    let coverTimeline = new TimelineMax();
+    TweenLite.set('.cover', {
+      x: '-120%'
+    })
+    coverTimeline.add('start');
+    coverTimeline.to('.cover', 2, {
+      x: '120%',
+      ease: SlowMo.ease.config(0.2, 0.5, false),
+      force3D: false
     });
-
-    requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
-  }
-
-  function onScroll() {
-    scroller.scrollRequest++;
-    if (!requestId) {
-      requestId = requestAnimationFrame(updateScroller);
+    let nextPage = getNewPageFile().url.split('/').pop();
+    if (nextPage === 'about.html') {
+      return AboutTransition;
+    } else if (nextPage === 'contact.html') {
+      return ContactTransition;
+    } else if (nextPage.includes('project')) {
+      return ProjectTransition;
     }
-  }
+    return CommonTransition;
+  };
 
-  function onResize() {
-    scroller.resizeRequest++;
-    if (!requestId) {
-      requestId = requestAnimationFrame(updateScroller);
-    }
-  }
 
+  Barba.Pjax.start();
 });
